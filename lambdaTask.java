855cbd1e8c88d7eb6aa1e17f79f9376a929256cf
ShikhaSharma20 <109273450+ShikhaSharma20@users.noreply.github.com>
2022-09-02 10:43:54 +0530

    Add files via upload

---
+++ lambdaTask.java
@@ -0,0 +1,39 @@
+/*to print the Divisible of 5 using of Stream Iterate, filter method, limit method and forEach method.
+ * by Shikha
+ */
+
+
+public class lambdaTask{
+
+    public static void main(String[] args) {
+        lambdaTask tester = new lambdaTask();
+
+        //with type declaration
+        MathOperation addition = (int a, int b) -> a + b;
+
+        //without type declaration
+        MathOperation subtraction = (a, b) -> a - b;
+
+        //with return statement along with curly braces
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        //without return statement and without curly braces
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("66 + 8 = " + tester.operate(addition));
+        System.out.println("66 - 8 = " + tester.operate(subtraction));
+        System.out.println("66 x 8 = " + tester.operate(multiplication));
+        System.out.println("66 / 8 = " + tester.operate(division));
+
+
+    }
+
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+
+    private int operate(MathOperation mathOperation) {
+        return mathOperation.operation(10, 5);
+    }
+}
---
+++ laptop.java
@@ -0,0 +1,23 @@
+package store.java;
+
+public class laptop {
+
+	int lid;
+    String lCompany;
+    String lName;
+    int lPrice;
+    int lModel;
+    public laptop(int lid, String lCompany,String lName,int lPrice,int lModel){
+        this. lid = lid;
+        this.lCompany = lCompany;
+        this.lName = lName;
+        this.lPrice = lPrice;
+        this.lModel = lModel;
+
+
+    }
+
+
+
+}
+		
